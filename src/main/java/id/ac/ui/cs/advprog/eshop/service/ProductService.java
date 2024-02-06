51e717fd2ce4d54702d90eeec3f0d26db0509cc8
marvelm57 <vivalamarvel@gmail.com>
2024-02-06 23:52:24 +0700

    Update ProductService.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,4 +6,8 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+
+    public Product findProduct(int id);
+
+    public Product editProduct(int id, Product product);
 }
