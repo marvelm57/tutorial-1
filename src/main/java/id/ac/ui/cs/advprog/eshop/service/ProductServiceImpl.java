0a3aa08b6a6f74954f5eaf1a0ea297da0f325aa4
marvelm57 <vivalamarvel@gmail.com>
2024-02-07 01:17:05 +0700

    Update ProductServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -28,4 +28,9 @@ public class ProductServiceImpl implements ProductService {
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
+
+    @Override
+    public void deleteProduct(int id) {
+        productRepository.deleteProduct(id);
+    }
 }
