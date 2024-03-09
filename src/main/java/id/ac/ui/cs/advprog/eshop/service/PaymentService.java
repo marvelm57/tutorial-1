edc446e45959bde323cd56fe9bc8e0c950730c2a
marvelm57 <vivalamarvel@gmail.com>
2024-03-09 16:56:26 +0700

    [REFACTOR] Modified PaymentServiceImpl skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface PaymentService {
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData);
+    public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData);
     public Payment setStatus(Payment payment, String status);
     public Payment getPayment(String paymentId);
     public List<Payment> getAllPayments();
